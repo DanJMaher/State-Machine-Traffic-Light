9ff26fc59aea225e50f4310822c7af42eb5b042f
Dan Maher <djm227@gmail.com>
2021-05-24 17:47:40 -0400

    Adjusted tabbing for correct github display

--- main.c
+++ main.c
@@ -23,31 +23,31 @@ struct State{
 	unsigned long next[8];
 };
 
-#define GoWest 				0
-#define WaitWest 			1
-#define GoSouth 			2
-#define WaitSouth 		3
-#define Walk 					4
-#define DontWalkOn0 	5
-#define DontWalkOff0 	6
-#define DontWalkOn1	 	7
-#define DontWalkOff1 	8
-#define DontWalkOn2 	9
+#define GoWest 0
+#define WaitWest 1
+#define GoSouth 2
+#define WaitSouth 3
+#define Walk 4
+#define DontWalkOn0 5
+#define DontWalkOff0 6
+#define DontWalkOn1	7
+#define DontWalkOff1 8
+#define DontWalkOn2 9
 #define DontWalkOff2 10
 
 
 struct State TrafficState[11] = {
-	{0x0C, 0x02, 1000, 	{GoWest, 				GoWest, 			WaitWest, 		WaitWest,			WaitWest, 		WaitWest, 		WaitWest, 		WaitWest}},
-	{0x14, 0x02, 500, 	{GoSouth, 			GoSouth,			GoSouth, 			GoSouth, 			Walk, 				Walk, 				Walk, 				Walk}},
-	{0x21, 0x02, 1000, 	{GoSouth, 			WaitSouth, 		GoSouth, 			WaitSouth, 		WaitSouth, 		WaitSouth, 		WaitSouth, 		WaitSouth}},
-	{0x22, 0x02, 500, 	{GoWest, 				GoWest, 			GoWest, 			GoWest, 			Walk, 				Walk, 				Walk, 				Walk}},
-	{0x24, 0x08, 1000, 	{DontWalkOn0, 	DontWalkOn0, 	DontWalkOn0, 	DontWalkOn0,	DontWalkOn0, 	DontWalkOn0, 	DontWalkOn0, 	DontWalkOn0}},
-	{0x24, 0x02, 200, 	{DontWalkOff0,	DontWalkOff0, DontWalkOff0, DontWalkOff0, DontWalkOff0, DontWalkOff0, DontWalkOff0, DontWalkOff0}},
-	{0x24, 0x00, 200, 	{DontWalkOn1, 	DontWalkOn1, 	DontWalkOn1, 	DontWalkOn1, 	DontWalkOn1, 	DontWalkOn1, 	DontWalkOn1, 	DontWalkOn1}},
-	{0x24, 0x02, 200, 	{DontWalkOff1, 	DontWalkOff1, DontWalkOff1, DontWalkOff1, DontWalkOff1, DontWalkOff1, DontWalkOff1, DontWalkOff1}},
-	{0x24, 0x00, 200,	 	{DontWalkOn2, 	DontWalkOn2, 	DontWalkOn2, 	DontWalkOn2, 	DontWalkOn2, 	DontWalkOn2, 	DontWalkOn2, 	DontWalkOn2}},
-	{0x24, 0x02, 200, 	{DontWalkOff2, 	DontWalkOff2, DontWalkOff2, DontWalkOff2, DontWalkOff2, DontWalkOff2, DontWalkOff2, DontWalkOff2}},
-	{0x24, 0x00, 200,		{GoWest, 				GoWest, 			GoSouth, 			GoWest, 			GoWest, 			GoWest, 			GoSouth, 			GoWest}},
+	{0x0C, 0x02, 1000, 	{GoWest, GoWest, WaitWest, WaitWest, WaitWest, WaitWest, WaitWest, WaitWest}},
+	{0x14, 0x02, 500, 	{GoSouth, GoSouth, GoSouth, GoSouth, Walk, Walk, Walk, Walk}},
+	{0x21, 0x02, 1000, 	{GoSouth, WaitSouth, GoSouth, WaitSouth, WaitSouth, WaitSouth, WaitSouth, WaitSouth}},
+	{0x22, 0x02, 500, 	{GoWest, GoWest, GoWest, GoWest, Walk, Walk, Walk, Walk}},
+	{0x24, 0x08, 1000, 	{DontWalkOn0, DontWalkOn0, DontWalkOn0, DontWalkOn0, DontWalkOn0, DontWalkOn0, DontWalkOn0, DontWalkOn0}},
+	{0x24, 0x02, 200, 	{DontWalkOff0, DontWalkOff0, DontWalkOff0, DontWalkOff0, DontWalkOff0, DontWalkOff0, DontWalkOff0, DontWalkOff0}},
+	{0x24, 0x00, 200, 	{DontWalkOn1, DontWalkOn1, DontWalkOn1, DontWalkOn1, DontWalkOn1, DontWalkOn1, DontWalkOn1, DontWalkOn1}},
+	{0x24, 0x02, 200, 	{DontWalkOff1, DontWalkOff1, DontWalkOff1, DontWalkOff1, DontWalkOff1, DontWalkOff1, DontWalkOff1, DontWalkOff1}},
+	{0x24, 0x00, 200,	 	{DontWalkOn2, DontWalkOn2, DontWalkOn2, DontWalkOn2, DontWalkOn2, DontWalkOn2, DontWalkOn2, DontWalkOn2}},
+	{0x24, 0x02, 200, 	{DontWalkOff2, DontWalkOff2, DontWalkOff2, DontWalkOff2, DontWalkOff2, DontWalkOff2, DontWalkOff2, DontWalkOff2}},
+	{0x24, 0x00, 200,		{GoWest, GoWest, GoSouth, GoWest, GoWest, GoWest, GoSouth, GoWest}},
 };
 
 unsigned long index = 0;
